583e98935de5614c30aa49214ff03e9d5ac48116
Kevin J Walters <kjw.github@gmail.com>
2021-01-20 17:18:15 +0000

    Tweaks to the padding of the command text/area to stop it moving around including new _padfirstgvline() function. Some minor pylinting.

--- library/visualisation/test-visualisation.py
+++ library/visualisation/test-visualisation.py
@@ -1,4 +1,4 @@
-### test-visualisation v0.6
+### test-visualisation v0.7
 
 ### MIT License
 
@@ -28,16 +28,17 @@
 ### to help understand the tests
 
 import math
+import re
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from graphviz import Digraph
 from dominate.tags import table, tr, td, font  ### html tag library
 
 ### This includes MockSerialArbitrary and PMS5003Simulator classes
 ### which are the ones that will be modified
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+### pylint: disable=wrong-import-position
 from tests.test_setup import *
 
 
@@ -94,7 +95,7 @@ class TestVisualizerGV:
                                             ("read", m2))
 
 
-    def _constructor_posthook(self, *args, **kwargs):
+    def _constructor_posthook(self, *args, **kwargs):  ### pylint: disable=unused-argument
 
         obj = args[0]
         ### kwargs["rx_buf_size"] is not always set so fetch the
@@ -122,7 +123,7 @@ class TestVisualizerGV:
         self.make_diagram()
 
 
-    def _read_prehook(self, obj, length=None):
+    def _read_prehook(self, obj, length=None):  ### pylint: disable=unused-argument
 
         bytes_to_read = min(length,
                             self._gv_buflen,
@@ -151,15 +152,15 @@ class TestVisualizerGV:
 
         ### \l is \n with left justification in graphviz
         read_cmd = "read({:s}) = ".format(len_as_text)
-        gv_cmd = (read_cmd + enc_bytes_as_text[0]
-                  + "".join(["\\l" + (" " * len(read_cmd))
-                             + l for l in enc_bytes_as_text[1:]]))
+        gv_cmd = (read_cmd + enc_bytes_as_text[0] + "\\l"
+                  + "".join([(" " * len(read_cmd))
+                             + l + "\\l" for l in enc_bytes_as_text[1:]]))
         self._gv_command = gv_cmd
         self.make_diagram()
         self._gv_buf_highlight = None
 
 
-    def _read_posthook(self, obj, length=None):
+    def _read_posthook(self, obj, length=None):  ### pylint: disable=unused-argument
 
         self._gv_buffer[:] = obj.buffer
         self._gv_buflen = obj.buflen
@@ -211,6 +212,22 @@ class TestVisualizerGV:
         return html_table.render(pretty=False)
 
 
+    @staticmethod
+    def _padfirstgvline(text, length):
+        eol = re.search("\\\\[nlr]", text)  ### NB: escaped doubleslash!
+        if eol:
+            first_eol_idx = eol.span()[0]
+            line1 = text[0:first_eol_idx]
+            rest = text[first_eol_idx:]
+        else:
+            line1 = text
+            rest = ""
+
+        return ((line1 if len(line1) >= length
+                 else (line1 + " " * (length - len(line1))))
+                + rest)
+
+
     def make_diagram(self):
         tl = Digraph('structs',
                      filename="frame-{:s}-{:04d}.gv".format(self._name,
@@ -230,23 +247,35 @@ class TestVisualizerGV:
                                          width=self._gv_buf_width,
                                          highlight=self._gv_buf_highlight)
 
-            d.node('buffer', "<\n" + gvbufrepr + ">", width="6")  ### html appears to go inside extra angle brackets
+            ### html appears to go inside extra angle brackets
+            d.node('buffer', "<\n" + gvbufrepr + ">", width="6")
 
+            ### whitespace edge labels help keep spacing consistent
             if self._serial_byte is None:
                 d.node('serial', self.NOBYTETEXT,
                        labelfontcolor="gray", style="rounded", shape="box")
+                d.edge("serial", "buffer",
+                       fontsize="10", label=" \\n ", style="invis", weight="10000")  ### helps with positioning
             else:
                 d.node('serial', "{:02x}".format(self._serial_byte),
                        style="rounded", shape="box", weight="0")
                 if self._gv_buf_highlight:
                     d.edges([("serial", "buffer:port_{:03d}:n".format(self._gv_buf_highlight[0]))])
+                    d.edge("serial", "buffer",
+                           fontsize="10", label=" \\n ", style="invis", weight="10000")  ### helps with positioning
+                else:
+                    ### Implies buffer is full
+                    d.edge("serial", "buffer", style="dashed", weight="10000",
+                           fontsize="10",  ### 14 is default
+                           label="  discarded\\n  buffer full", arrowhead="none")
 
-            d.edge("serial", "buffer", style="invis", weight="10000")  ### helps with positioning
-            #d.node('command', 'read({:d}) = "aabbcc"'.format(32), color="white")
-
-            ### TODO - maybe construct command box so it always has same width 14+4*8  ?
-
-            d.node('command', self._gv_command if self._gv_command else " \n \n \n ", shape="none")
+            ### Padding here is a simple way to maintain size and position
+            ### The blank command is peculiar in neededing a space character
+            ### to get matching height, hence " \\n" per line
+            gv_cmd = self._gv_command if self._gv_command else " \\n" * 4
+            d.node('command',
+                   self._padfirstgvline(gv_cmd, 14 + 4 * 8),
+                   shape="none")
 
             if self._gv_buf_highlight and self._gv_command:
                 d.edge("buffer:" + "port_000:w", "command:w", color="blue", weight="0")
